820e2f30c74eba491c784b57c0d58f5f16548ad0
Raphsodyz <arthurvsr@gmail.com>
2026-05-13 22:35:08 -0300

    Feat: Added di fpr usecase for search

--- usecase/search_launch_usecase.go
+++ usecase/search_launch_usecase.go
@@ -1,1 +1,29 @@
 package usecase
+
+import (
+	repository "github.com/Raphsodyz/spacedevs-go/repository"
+)
+
+type SearchLaunchUseCase struct {
+	missionRepo       repository.MissionRepository
+	configurationRepo repository.ConfigurationRepository
+	locationRepo      repository.LocationRepository
+	padRepo           repository.PadRepository
+	launchRepo        repository.LaunchRepository
+}
+
+func NewSearchLaunchUseCase(
+	missionRepo repository.MissionRepository,
+	configurationRepo repository.ConfigurationRepository,
+	locationRepo repository.LocationRepository,
+	padRepo repository.PadRepository,
+	launchRepo repository.LaunchRepository,
+) *SearchLaunchUseCase {
+	return &SearchLaunchUseCase{
+		missionRepo:       missionRepo,
+		configurationRepo: configurationRepo,
+		locationRepo:      locationRepo,
+		padRepo:           padRepo,
+		launchRepo:        launchRepo,
+	}
+}

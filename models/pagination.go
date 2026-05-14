213be60c6dfad6ade7a9ca54e2c276b5356c9cdc
Raphsodyz <arthurvsr@gmail.com>
2026-05-13 23:48:42 -0300

    Feat: Joined all struct of search in the same file.

--- models/pagination.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Pagination[T any] struct {
-	Entities         []T
-	NumberOfPages    int
-	CurrentPage      int
-	NumberOfEntities int
-}
--- models/search_launch.go
+++ models/search_launch.go
@@ -1,9 +1,26 @@
 package models
 
+type SearchLaunchFilters struct {
+	MissionIds  []int64
+	RocketIds   []int64
+	LocationIds []int64
+	PadIds      []int64
+	LaunchIds   []int64
+	Page        int
+}
+
 type SearchLaunchRequest struct {
 	Mission  *string `json:"mission" validate:"omitempty,lte=360"`
 	Rocket   *string `json:"rocket" validate:"omitempty,lte=360"`
 	Location *string `json:"location" validate:"omitempty,lte=360"`
 	Pad      *string `json:"pad" validate:"omitempty,lte=360"`
+	Launch   *string `json:"launch" validate:"omitempty,lte=360"`
 	Page     *int    `json:"page" validate:"omitempty,gte=0"`
+}
+
+type SearchLaunchResponse[T any] struct {
+	Entities         []T
+	NumberOfPages    int
+	CurrentPage      int
+	NumberOfEntities int
 }
